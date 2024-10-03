96be4e1aa083ab97193b8c0bd59444732650559d
Benicio Cardozo <beniciocardozomdp@gmail.com>
2024-10-03 00:05:04 -0300

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,43 @@ export default defineNuxtConfig({
     editorSupport: true
     // editorSupport: { autocompleteUtil: { as: 'tailwindClasses' } }
   },
+  pwa: {
+      registerType: 'autoUpdate',
+      manifest: {
+      name: 'Croissant App',
+      short_name: 'Crossi',
+      description: 'Aplicación de puntos del Café Croissant',
+            lang:'es',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'images/logo_croissant_pwa.png',
+          type: 'image/png',
+                    sizes: '144x144',
+        },
+       
+      ],
+    },
+          workbox: {
+
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      // suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  },
    runtimeConfig: {
     FUDO_API_KEY: '',
 FUDO_API_SECRET: '',
